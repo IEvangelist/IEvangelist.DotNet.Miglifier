203a1d68fca4d838ea2281b7e2e4116ee3773283
David Pine <david.pine.7@gmail.com>
2018-10-10 14:49:11 -0500

    Add project files.

---
+++ IEvangelist.DotNet.Miglifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IEvangelist.DotNet.Miglifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
